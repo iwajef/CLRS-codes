5a6b6831fcc4f20b647ff13c4eb5ad770b557dbe
iwajef <iwajef@foxmail.com>
2020-09-22 14:14:15 +0800

    edit

--- Chapter 2/InsertionSort.cpp
+++ Chapter 2/InsertionSort.cpp
@@ -1,8 +1,7 @@
 
-/* 
- * @Author: iwajef
- * @Date: 2020-09-20
- * 
+/*
+ * Author: iwajef
+ * Date: 2020-09-20
  */
 
 #include <cstdio>
@@ -13,49 +12,42 @@ void PrintArray(int* arr, int n);
 
 int main()
 {
-	int arr[10] = {4, 2, 7, 8, 1, 9, 3, 10, 5, 6};  // an array that needs to be sorted
-	
-	printf("The original array: \n");
-	PrintArray(arr, 10);
-	
-	InsertionSort(arr, 10);  // use the insertion sort
-	
-	printf("The array after sorted: \n");
-	PrintArray(arr, 10);
-	
-	return 0;
+    int arr[10] = {4, 2, 7, 8, 1, 9, 3, 10, 5, 6};
+
+    printf("The original array: \n");
+    PrintArray(arr, 10);
+
+    InsertionSort(arr, 10);
+
+    printf("The array after sorted: \n");
+    PrintArray(arr, 10);
+
+    return 0;
 }
 
 
 void InsertionSort(int* arr, int n)
 {
-	// arr[0...i-1] are already sorted
-	// insert arr[i] at the correct index
-	for (int i = 1; i < n; i++)
-	{
-		int key = arr[i];  // key is the current value that is gonna insert to arr[0...i-1]
-		int j = i - 1;
-		// iterate from i-1 through correct index
-		while (j >= 0 && key < arr[j])  // key < arr[j] sorting in non-descending order
-		{
-			arr[j + 1] = arr[j];  // move the values next to them
-			j--;  // j-1 continue the loop
-		}
-		arr[j + 1] = key;  // j+1 is the correct index to insert the key. PERFECT!!!
-	}
-	// Time Complexity: O(n^2)
-	// Worst Case: O(n^2)
-	// Best Case: O(n), the array is already in sorted order, just have to scan the whole array once
-	// Space Complexity: O(1)
+    // arr[0 ... i-1] are already sorted
+    // insert arr[i] at the correct index
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];  // key is the current value that is going to be inserted to arr[0 ... i-1]
+        int j = i - 1;
+        // iterate from i-1 through correct index
+        while (j >= 0 && key < arr[j])  // key < arr[j] means non-descending order
+        {
+            arr[j + 1] = arr[j];  // move the values next to them
+            j--;
+        }
+        arr[j + 1] = key;  // j+1 is the correct index to insert the key
+    }
 }
 
 
 void PrintArray(int* arr, int n)
 {
-	for (int i = 0; i < n - 1; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("%d\n", arr[n - 1]);
+    for (int i = 0; i < n - 1; i++)
+        printf("%d ", arr[i]);
+    printf("%d\n", arr[n - 1]);
 }
-
--- Chapter 2/MergeSort.cpp
+++ Chapter 2/MergeSort.cpp
@@ -1,93 +1,83 @@
 
-/* 
- * @Author: iwajef
- * @Date: 2020-09-20
- * 
+/*
+ * Author: iwajef
+ * Date: 2020-09-20
  */
 
 #include <cstdio>
 #include <cstdlib>
 #include <climits>
 
-void MergeSort(int* arr, int left, int right);
-void Merge(int* arr, int left, int mid, int right);
+void MergeSort(int* arr, int p, int r);
+void Merge(int* arr, int p, int q, int r);
 void PrintArray(int* arr, int n);
 
 
 int main()
 {
-	int arr[10] = {4, 2, 7, 8, 1, 9, 3, 10, 5, 6};
-	
-	printf("The original array: \n");
-	PrintArray(arr, 10);
-	
-	MergeSort(arr, 0, 9);  // use the merge sort
-	
-	printf("The array after sorted: \n");
-	PrintArray(arr, 10);
-	
-	return 0;
+    int arr[10] = {4, 2, 7, 8, 1, 9, 3, 10, 5, 6};
+
+    printf("The original array: \n");
+    PrintArray(arr, 10);
+
+    MergeSort(arr, 0, 9);
+
+    printf("The array after sorted: \n");
+    PrintArray(arr, 10);
+
+    return 0;
 }
 
 
-void MergeSort(int* arr, int left, int right)
+void MergeSort(int* arr, int p, int r)
 {
-	if (left < right)
-	{
-		int mid = (left + right) / 2;
-		// divide the original array to [left ... mid] / [mid+1 ... right] parts and sort separately
-		MergeSort(arr, left, mid);
-		MergeSort(arr, mid + 1, right);
-		// merge two sorted arrays
-		Merge(arr, left, mid, right);
-	}
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        // divide the original array to [p ... q] and [q+1 ... r]
+        MergeSort(arr, p, q);
+        MergeSort(arr, q + 1, r);
+        // merge two sorted arrays
+        Merge(arr, p, q, r);
+    }
 }
 
 
-void Merge(int* arr, int left, int mid, int right)
+void Merge(int* arr, int p, int q, int r)
 {
-	int n1 = mid - left + 1;  // n1, n2 is the element number of arrays L and R
-	int n2 = right - mid;
-	
-	// L.size: arr[left ... mid] + 1(infinite)
-	// R.size: arr[mid+1 ... right] + 1(infinite) 
-	// DO NOT initialise like this in C: int L[n1 + 1], R[n2 + 1];
-	int* L = (int*)malloc(sizeof(int) * (n1 + 1));
-	int* R = (int*)malloc(sizeof(int) * (n2 + 1));
-	for (int i = 0; i < n1; i++)
-		L[i] = arr[left + i];
-	for (int i = 0; i < n2; i++)
-		R[i] = arr[mid + 1 + i];
-	L[n1] = INT_MAX;
-	R[n2] = INT_MAX;
-	
-	// Merge two arrays(L and R) to the original array 'arr' using two pointers
-	int i = 0;
-	int j = 0;
-	for (int k = left; k <= right; k++)
-	{
-		if (L[i] <= R[j])  // non-descending order
-		{
-			arr[k] = L[i];
-			i++;
-		}
-		else
-		{
-			arr[k] = R[j];
-			j++;
-		}
-	}
-	// Time Complexity: O(nlogn)
-	// Space Complexity: O(n) (new arrays 'L' and 'R')
+    // n1, n2 is the size of arrays L, R
+    int n1 = q - p + 1;
+    int n2 = r - q;
+    // don't forget to add the size by 1 to append additional INT_MAX
+    int* L = (int*)malloc(sizeof(int) * (n1 + 1));
+    int* R = (int*)malloc(sizeof(int) * (n2 + 1));
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[q + 1 + i];
+    L[n1] = INT_MAX; R[n2] = INT_MAX;
+
+    // Merge two arrays L, R to the original array (arr)
+    int i = 0; int j = 0;
+    for (int k = p; k <= r; k++)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+    }
 }
 
 
 void PrintArray(int* arr, int n)
 {
-	for (int i = 0; i < n - 1; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("%d\n", arr[n - 1]);
+    for (int i = 0; i < n - 1; i++)
+        printf("%d ", arr[i]);
+    printf("%d\n", arr[n - 1]);
 }
-
